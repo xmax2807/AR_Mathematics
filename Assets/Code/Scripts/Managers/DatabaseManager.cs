934df930188fb380835bf2ec091683a3397e5e63
19127445 <64112482+19127445@users.noreply.github.com>
2023-02-28 11:00:09 +0700

    Connect to MongoDB

--- Assets/Code/Scripts/Managers/DatabaseManager.cs
+++ Assets/Code/Scripts/Managers/DatabaseManager.cs
@@ -1,3 +1,23 @@
 using UnityEngine;
-public class DataBaseManager : MonoBehaviour{
+using MongoDB.Driver;
+
+
+public class DatabaseManager : MonoBehaviour
+{
+    MongoClient client = new MongoClient(
+        "mongodb+srv://khoa:khoa@cluster0.gwp7sx0.mongodb.net/?retryWrites=true&w=majority"
+    );
+    IMongoDatabase database;
+    IMongoCollection<UserModel> collection;
+
+    void Start() {
+        database = client.GetDatabase("Math");
+        collection = database.GetCollection<UserModel>("User");
+        UserModel model;
+        model = new UserModel(){
+            UserID = "asd",
+            
+        };
+        collection.InsertOne(model);
+     }
 }
---
+++ Assets/Code/Scripts/ModelClasses/UserModel.cs
@@ -0,0 +1,12 @@
+using MongoDB.Bson;
+using Realms;
+
+public class UserModel : RealmObject
+{
+    public string UserID { get; set; }
+    public string Username{get;set;}
+    public string Password{get;set;}
+    // public  CreatedAt {get;set;}
+    // public string UpdatedAt {get;set;}
+
+}
