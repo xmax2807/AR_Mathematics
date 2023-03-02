b310bdfa454e5950c50303104ae70aaaf7058bd2
19127445 <64112482+19127445@users.noreply.github.com>
2023-03-02 10:23:58 +0700

    Add User to Server

--- Assets/Code/Scripts/API/UserController.cs
+++ Assets/Code/Scripts/API/UserController.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 using MongoDB.Driver;
+using System;
 
-public class UserController : MonoBehaviour {
-     MongoClient client = new MongoClient(
-        "mongodb+srv://khoa:khoa@cluster0.gwp7sx0.mongodb.net/?retryWrites=true&w=majority"
-    );
+public class UserController : MonoBehaviour
+{
+    MongoClient client = new MongoClient(
+       "mongodb+srv://khoa:khoa@cluster0.gwp7sx0.mongodb.net/?retryWrites=true&w=majority"
+   );
     IMongoDatabase database;
     IMongoCollection<UserModel> userCollection;
 
-    private UserModel addUser(string rawJson){
-        var newUser = new UserModel();
-        return newUser;
+    private void Start()
+    {
+        System.Globalization.CultureInfo.CurrentCulture.ClearCachedData();
+        database = client.GetDatabase("Math");
+        userCollection = database.GetCollection<UserModel>("User");
+        Send("xmax", "pass");
+    }
+    public void Send(string username, string password)
+    {
+        UserModel user = new UserModel();
+        user.Username = username;
+        user.Password = password;
+        user.CreatedAt = DateTime.Now;
+        user.UpdatedAt = DateTime.Now;
+
+        AddUser(user);
+    }
+    private async void AddUser(UserModel user)
+    {
+        await userCollection.InsertOneAsync(user);
     }
 }
--- Assets/Code/Scripts/ModelClasses/UserModel.cs
+++ Assets/Code/Scripts/ModelClasses/UserModel.cs
@@ -2,10 +2,10 @@ using MongoDB.Bson;
 
 public class UserModel
 {
-    public string UserID { get; set; }
+    // public string UserID { get; set; }
     public string Username{get;set;}
     public string Password{get;set;}
-    public  BsonTimestamp CreatedAt {get;set;}
-    public BsonTimestamp UpdatedAt {get;set;}
+    public  BsonDateTime CreatedAt {get;set;}
+    public BsonDateTime UpdatedAt {get;set;}
 
 }
