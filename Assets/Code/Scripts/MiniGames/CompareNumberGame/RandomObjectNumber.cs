c80dd15fef8d86cfd691481d309745769990c085
Minhgunter <hongminhau2011@gmail.com>
2023-05-20 10:49:59 +0700

    update spawning object

--- Assets/Code/Scripts/MiniGames/CompareNumberGame/RandomObjectNumber.cs
+++ Assets/Code/Scripts/MiniGames/CompareNumberGame/RandomObjectNumber.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Project.Managers;
 using UnityEngine.Events;
+using UnityEngine.UIElements;
+using System;
+using Random = UnityEngine.Random;
 
 public class RandomObjectNumber : MonoBehaviour
 {
@@ -11,6 +14,10 @@ public class RandomObjectNumber : MonoBehaviour
 	[SerializeField]
 	public TMPro.TextMeshProUGUI firstNumber;
 	public TMPro.TextMeshProUGUI secondNumber;
+	public TMPro.TextMeshProUGUI comparision;
+
+	public int RandomNumberRange;
+
 
 	[SerializeField] private Transform parentFirstObjTransform;
 	[SerializeField] private Transform parentSecondObjTransform;
@@ -21,50 +28,145 @@ public class RandomObjectNumber : MonoBehaviour
 	public Vector3 rotateObject;
 
 	int[] randomNumbers = new int[2];
+
+	public GameObject spawnedObject { get; private set; }
+
 	void Start()
 	{
-		randomNumbers[0] = Random.Range(0, 100);
-		randomNumbers[1] = Random.Range(0, 100);
+		randomNumbers[0] = Random.Range(0, 20);
+		randomNumbers[1] = Random.Range(0, 20);
 		firstObject = objects[Random.Range(0, objects.Count)];
 		secondObject = objects[Random.Range(0, objects.Count)];
-
 	}
 
-	// Update is called once per frame
-	void Update()
+	//void Awake()
+	//{
+	//	if (Input.GetMouseButtonDown(0))
+	//	{
+	//		Start();
+
+	//		if (firstNumber.text != randomNumbers[0].ToString() & secondNumber.text != randomNumbers[1].ToString())
+	//		{
+	//			firstNumber.text = randomNumbers[0].ToString();
+	//			secondNumber.text = randomNumbers[1].ToString();
+	//		}
+
+	//		if (firstObject != null)
+	//		{
+	//			for (int i = 0; i < randomNumbers[0]; i++)
+	//			{
+	//				float y = i / 10;
+	//				float x = i % 10;
+	//				Instantiate(firstObject, parentFirstObjTransform);
+	//				var position = firstObject.transform.position;
+	//				position.x = position.x + x;
+	//				position.y = position.y + y;
+	//				firstObject.transform.position = position;
+	//				var rotation = firstObject.transform.rotation;
+	//				firstObject.transform.Rotate(rotation.x + rotateObject.x, rotation.y + rotateObject.y, rotation.z + rotateObject.z);
+	//				Debug.Log("First spawned: " + i + " " + firstObject.transform.position + " " + firstObject.transform.rotation);
+	//			}
+	//		}
+	//		if (secondObject != null)
+	//		{
+	//			for (int i = 0; i < randomNumbers[1]; i++)
+	//			{
+	//				float x = i / 10;
+	//				float y = i % 10;
+	//				Instantiate(secondObject, parentSecondObjTransform);
+	//				var position = secondObject.transform.position;
+	//				position.x = position.x + x;
+	//				position.y = position.y + y;
+	//				secondObject.transform.position = position;
+	//				var rotation = secondObject.transform.rotation;
+	//				secondObject.transform.Rotate(rotation.x + rotateObject.x, rotation.y + rotateObject.y, rotation.z + rotateObject.z);
+	//				Debug.Log("Second spawned: " + i + " " + secondObject.transform.position + " " + secondObject.transform.rotation);
+	//			}
+	//		}
+	//	}
+	//	if (Input.GetMouseButton(0))
+	//	{
+	//		if (randomNumbers[0] > randomNumbers[1])
+	//		{
+	//			comparision.text = ">";
+	//		}
+	//		else if (randomNumbers[0] < randomNumbers[1])
+	//		{
+	//			comparision.text = "<";
+	//		}
+	//		else
+	//		{
+	//			comparision.text = "=";
+	//		}
+	//	}
+	//}
+	//Update is called once per frame
+
+void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
 			Start();
-			firstNumber.text = randomNumbers[0].ToString();
-			secondNumber.text = randomNumbers[1].ToString();
-			firstObject.SetActive(true);
-			secondObject.SetActive(true);
-		}
 
-		if (firstObject != null)
-		{
-			for (int i = 0; i < randomNumbers[0]; i++)
+			if (firstNumber.text != randomNumbers[0].ToString() & secondNumber.text != randomNumbers[1].ToString())
 			{
-				Instantiate(firstObject, parentFirstObjTransform);
-				//firstObject.transform.LookAt(ARCamera.transform);
-				var rotation = firstObject.transform.rotation;
-				firstObject.transform.Rotate(rotation.x + rotateObject.x, rotation.y + rotateObject.y, rotation.z + rotateObject.z);
+				firstNumber.text = randomNumbers[0].ToString();
+				secondNumber.text = randomNumbers[1].ToString();
+			}
+
+			if (firstObject != null)
+			{
+				for (int i = 0; i < randomNumbers[0]; i++)
+				{
+					float y = i / 10;
+					float z = i % 10;
+					Instantiate(firstObject, parentFirstObjTransform);
+					var position = firstObject.transform.position;
+					position.z = z + parentFirstObjTransform.position.z;
+					position.y = y + 1;
+					firstObject.transform.position = position;
+					var rotation = firstObject.transform.rotation;
+					//firstObject.transform.Rotate(rotateObject.x, rotateObject.y, rotateObject.z);
+					Debug.Log("First spawned: " + i + " " + firstObject.transform.position + " " + firstObject.transform.rotation);
+				}
+			}
+			if (secondObject != null)
+			{
+				for (int i = 0; i < randomNumbers[1]; i++)
+				{
+					float y = i / 10;
+					float z = i % 10;
+					Instantiate(secondObject, parentSecondObjTransform);
+					var position = secondObject.transform.position;
+					position.z = z + parentSecondObjTransform.position.z;
+					position.y = y + 1;
+					secondObject.transform.position = position;
+					var rotation = secondObject.transform.rotation;
+					//secondObject.transform.Rotate(rotateObject.x, rotateObject.y, rotateObject.z);
+					Debug.Log("Second spawned: " + i + " " + secondObject.transform.position + " " + secondObject.transform.rotation);
+				}
+			}
+			Destroy(firstObject, 5);
+			Destroy(secondObject, 5);
+		}
+		if (Input.GetMouseButton(0))
+		{
+			if (randomNumbers[0] > randomNumbers[1])
+			{
+				comparision.text = ">";
+			}
+			else if (randomNumbers[0] < randomNumbers[1])
+			{
+				comparision.text = "<";
+			}
+			else
+			{
+				comparision.text = "=";
 			}
 		}
-		if (secondObject != null)
-		{
-			for (int i = 0; i < randomNumbers[1]; i++)
-			{
-				Instantiate(secondObject, parentSecondObjTransform);
-				//firstObject.transform.LookAt(ARCamera.transform);
-				var rotation = secondObject.transform.rotation;
-				secondObject.transform.Rotate(rotation.x + rotateObject.x, rotation.y + rotateObject.y, rotation.z + rotateObject.z);
-			}
-		}
-		firstObject.SetActive(false);
-		secondObject.SetActive(false);
+		
 		placementUpdate.Invoke();
+
 	}
 }
 
