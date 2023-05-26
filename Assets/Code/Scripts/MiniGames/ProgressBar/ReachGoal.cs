928277612b8cad70791a874ba7e00101d778fbb1
Minhgunter <hongminhau2011@gmail.com>
2023-05-26 10:13:23 +0700

    update endscreen

--- Assets/Code/Scripts/MiniGames/ProgressBar/ReachGoal.cs
+++ Assets/Code/Scripts/MiniGames/ProgressBar/ReachGoal.cs
@@ -7,18 +7,30 @@ using UnityEngine.UI;
 public class ReachGoal : MonoBehaviour
 {
 	public Slider slider;
-	public GameObject objectToActivate;
-	public GameObject objectToDeactivate;
+	public List<GameObject> objectToActivate;
+	public List<GameObject> objectToDeactivate;
 
 	void Update()
 	{
-		objectToActivate.SetActive(false);
-		objectToDeactivate.SetActive(true);
+		foreach (GameObject obj in objectToActivate)
+		{
+			obj.SetActive(false);
+		}
+		foreach (GameObject obj in objectToDeactivate)
+		{
+			obj.SetActive(true);
+		}
 
 		if (slider.value == slider.maxValue)
 		{
-			objectToActivate.SetActive(true);
-			objectToDeactivate.SetActive(false);
+			foreach (GameObject obj in objectToActivate)
+			{
+				obj.SetActive(true);
+			}
+			foreach (GameObject obj in objectToDeactivate)
+			{
+				obj.SetActive(false);
+			}
 		}
 	}
 }
