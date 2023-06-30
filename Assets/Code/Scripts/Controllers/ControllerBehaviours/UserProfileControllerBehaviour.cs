89d23a882b8caa1c4f98fdabc3885d57615c56f8
xmax2807 <19127425@student.hcmus.edu.vn>
2023-06-30 21:36:16 +0700

    Fix button not direct to correct destination

--- Assets/Code/Scripts/Controllers/ControllerBehaviours/UserProfileControllerBehaviour.cs
+++ Assets/Code/Scripts/Controllers/ControllerBehaviours/UserProfileControllerBehaviour.cs
@@ -7,10 +7,12 @@ using Gameframe.GUI.PanelSystem;
 using System;
 using Project.UI.Panel.Form;
 using Project.Managers;
+using Project.UI.Panel;
 
 public class UserProfileControllerBehaviour : MonoBehaviour{
     private const string SceneName = "LoginScene";
     [SerializeField] SingleSceneLoadBehaviour SceneLoadBehaviour;
+    [SerializeField] PanelType notificationPanelViewType;
     [SerializeField] PanelStackSystem stackSystem;
     [SerializeField] Button SignOutButton;
     [SerializeField] Button DeleteAccountButton;
@@ -18,6 +20,7 @@ public class UserProfileControllerBehaviour : MonoBehaviour{
     [SerializeField] private PanelType Reconfirm;
     private TextFormPanelView passwordFormView;
     private PanelViewController<TextFormPanelView> panelViewController;
+    private OkCancelPanelViewController notificationController;
     
     #region User Account
     private bool accessGranted;
@@ -37,11 +40,12 @@ public class UserProfileControllerBehaviour : MonoBehaviour{
 
     private async void DeleteAccount()
     {
-        panelViewController = new(Reconfirm);
+        panelViewController ??= new(Reconfirm);
         await stackSystem.PushAsync(panelViewController);
         passwordFormView = panelViewController.View;
-        passwordFormView.SetTitle(CurrentUser.Email);
+        passwordFormView.SetTitle($"Email: {CurrentUser.Email}");
         passwordFormView.onUserConfirmed += HandleAccess;
+        passwordFormView.onCancel += stackSystem.Pop;
     }
 
     private async void HandleAccess(string[] data)
@@ -53,15 +57,35 @@ public class UserProfileControllerBehaviour : MonoBehaviour{
         if(accessGranted == false){
             //Pop fail
             Debug.Log($"Failed to access: {CurrentUser.Email}");
-            stackSystem.Pop();
+            
+            if(notificationController != null && notificationController.IsViewLoaded){
+                Destroy(notificationController.View.gameObject);
+            }
+
+            notificationController = new(notificationPanelViewType, (isOk)=>{
+                stackSystem.Pop();
+            });
+
+            await stackSystem.PushAsync(notificationController);
+            NotificationPanelView view = (NotificationPanelView)notificationController.View;
+            view.SetUI("Mật khẩu bạn nhập không đúng", "Sai mật khẩu");
             return;
         }
         else{
 
             //for now, return to login scene
             await UserController.DeleteCurrentUser();
-            stackSystem.Pop();
-            LoadScene(SceneName);
+            //stackSystem.Pop();
+            if(notificationController != null && notificationController.IsViewLoaded){
+                Destroy(notificationController.View.gameObject);
+            }
+            notificationController = new(notificationPanelViewType, (isOk)=>{
+                stackSystem.Pop();    
+                LoadScene(SceneName);
+            });
+            await stackSystem.PushAsync(notificationController);
+            NotificationPanelView view = (NotificationPanelView)notificationController.View;
+            view.SetUI("Xóa tài khoản thành công", "Thông báo");
             //await stackSystem.PopAndPushAsync(1, new PanelViewController(Reconfirm));
         }
         
